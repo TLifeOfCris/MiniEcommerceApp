83b7168ced8ec0c94dd1dfc3a04018189de58a07
TLifeOfCris <crissandoval150201@gmail.com>
2025-09-23 17:08:38 -0600

    Se completó el Create Account screen

--- lib/screens/logincreataccountscreens/createaccount.dart
+++ lib/screens/logincreataccountscreens/createaccount.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miniecommerceapp/widgets/buttonfromwelcome.dart';
 import 'package:miniecommerceapp/widgets/createfield.dart';
 
 class CreateAnAccountScreen extends StatelessWidget {
@@ -7,7 +8,9 @@ class CreateAnAccountScreen extends StatelessWidget {
   final TextEditingController confirmEmailController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController nameController = TextEditingController();
-  final TextEditingController direccionController = TextEditingController();
+  final TextEditingController countryController = TextEditingController();
+  final TextEditingController streetController = TextEditingController();
+  final TextEditingController postalCodeController = TextEditingController();
    CreateAnAccountScreen({super.key});
 
   @override
@@ -26,6 +29,18 @@ class CreateAnAccountScreen extends StatelessWidget {
               Text('CREATE AN ACCOUNT'),
               Text('Crea una cuenta y comienza a comprar'),
               SizedBox(height: 20,),
+              Createfield(icon: Icons.email,
+               iconColor: Color(0xFFF97316),
+               labelText: 'Full Name',
+               colorLabelText: Color(0xFF1F2937),
+               hintText: 'Full Name', 
+               colorHintText: Color(0xFF6B7280), 
+               fillColor: Color(0xFFF3F4F6), 
+               borderColor: Color(0xFFF97316), 
+               bordersideColor: Color(0xFF1F2937), 
+               controller: nameController, 
+               textStyleColor: Color(0xFF111827)),
+               SizedBox(height: 20,),
               //checar si es mejor sizedbox o container
               Createfield(icon: Icons.email,
                iconColor: Color(0xFFF97316),
@@ -38,7 +53,9 @@ class CreateAnAccountScreen extends StatelessWidget {
                bordersideColor: Color(0xFF1F2937), 
                controller: emailController, 
                textStyleColor: Color(0xFF111827)),
+
               SizedBox(height: 20,),
+
               Createfield(icon: Icons.email,
                iconColor: Color(0xFFF97316),
                labelText: 'Confirm e-mail',
@@ -50,7 +67,23 @@ class CreateAnAccountScreen extends StatelessWidget {
                bordersideColor: Color(0xFF1F2937), 
                controller: emailController, 
                textStyleColor: Color(0xFF111827)),
+
                SizedBox(height: 20,),
+
+              Createfield(icon: Icons.password,
+               iconColor: Color(0xFFF97316),
+               labelText: 'Password',
+               colorLabelText: Color(0xFF1F2937),
+               hintText: 'Password', 
+               colorHintText: Color(0xFF6B7280), 
+               fillColor: Color(0xFFF3F4F6), 
+               borderColor: Color(0xFFF97316), 
+               bordersideColor: Color(0xFF1F2937), 
+               controller: passwordController, 
+               textStyleColor: Color(0xFF111827)),
+
+               SizedBox(height: 20,),
+
               Createfield(
                 icon: Icons.phone, 
                 iconColor: Color(0xFFF97316), 
@@ -64,7 +97,60 @@ class CreateAnAccountScreen extends StatelessWidget {
                 controller: phoneNumberController, 
                 textStyleColor: Color(0xFF111827),
                 isNumeric: true,
-                )
+                ),
+
+                SizedBox(height: 20,),
+                Createfield(
+                icon: Icons.home, 
+                iconColor: Color(0xFFF97316), 
+                labelText: 'Example: Okuurt st 123', 
+                colorLabelText: Color(0xFF1F2937), 
+                hintText: 'Street Addres', 
+                colorHintText: Color(0xFF6B7280), 
+                fillColor: Color(0xFFF3F4F6), 
+                borderColor: Color(0xFFF97316), 
+                bordersideColor: Color(0xFF1F2937), 
+                controller: streetController, 
+                textStyleColor: Color(0xFF111827),
+                isNumeric: false,
+                ),
+                SizedBox(height: 20,),
+                Createfield(
+                icon: Icons.location_city, 
+                iconColor: Color(0xFFF97316), 
+                labelText: 'Country', 
+                colorLabelText: Color(0xFF1F2937), 
+                hintText: 'Country', 
+                colorHintText: Color(0xFF6B7280), 
+                fillColor: Color(0xFFF3F4F6), 
+                borderColor: Color(0xFFF97316), 
+                bordersideColor: Color(0xFF1F2937), 
+                controller: countryController, 
+                textStyleColor: Color(0xFF111827),
+                isNumeric: false,
+                ),
+                SizedBox(height: 20,),
+                Createfield(
+                icon: Icons.markunread_mailbox, 
+                iconColor: Color(0xFFF97316), 
+                labelText: 'Postal Code', 
+                colorLabelText: Color(0xFF1F2937), 
+                hintText: 'Postal Code', 
+                colorHintText: Color(0xFF6B7280), 
+                fillColor: Color(0xFFF3F4F6), 
+                borderColor: Color(0xFFF97316), 
+                bordersideColor: Color(0xFF1F2937), 
+                controller: postalCodeController, 
+                textStyleColor: Color(0xFF111827),
+                isNumeric: true,
+                ),
+                SizedBox(height: 30,),
+
+                ButtonFromWelcome(texteo: 'Register', color: Colors.blue, size: 27, onPressed: (){}, textColor: Colors.white)
+
+                
+
+
         
               
             ],
